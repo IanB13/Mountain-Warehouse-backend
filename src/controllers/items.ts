6c1ad09e0f48ce3d9db57d114391c9b7cc512f24
IanB13 <48295744+IanB13@users.noreply.github.com>
2020-11-05 11:00:02 +0000

    Added get find Items by tag route

--- src/controllers/items.ts
+++ src/controllers/items.ts
@@ -5,7 +5,19 @@ const itemRouter = express.Router();
 
 itemRouter.post('/',  async (request, response) => {
     await Item.create(request.body);
-    response.status(200).json(request.body);
+    response.status(200).send(request.body);
+});
+
+itemRouter.get('/', async (request, response) => {
+    console.log(request.query);
+    const tags = request.query.tags;
+    if(tags){
+        const items = await Item.find( { "tags": { "$in": ["Cold"] } });
+        response.status(200).json(items);
+    }
+    else{
+        console.log("tags must be specified");
+    }
 });
 
 export default itemRouter;
